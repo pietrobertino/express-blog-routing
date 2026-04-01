1f1d29604e8e167d3f03b35d82204865003083a8
pietrobertino <pietrobertino20@gmail.com>
2026-04-01 16:58:05 +0200

    add show post logic

--- routers/posts.js
+++ routers/posts.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const port = 3020;
 
 
 
 const posts = [
-    { title: "Primo post", body: "Questo è il contenuto del primo post", img: "http://localhost:${port}/imgs/city.jpg", tags: ["tech", "programming"] },
-    { title: "Secondo post", body: "Questo è il contenuto del secondo post", img: "http://localhost:${port}/imgs/grass.jpg", tags: ["lifestyle", "travel"] },
-    { title: "Terzo post", body: "Questo è il contenuto del terzo post", img: "http://localhost:${port}/imgs/man.jpg", tags: ["food", "cooking"] },
-    { title: "Quarto post", body: "Questo è il contenuto del quarto post", img: "http://localhost:${port}/imgs/sea.jpg", tags: ["fitness", "health"] },
-    { title: "Quinto post", body: "Questo è il contenuto del quinto post", img: "http://localhost:${port}/imgs/street.jpg", tags: ["fashion", "style"] }
+    { id: 1, title: "Primo post", body: "Questo è il contenuto del primo post", img: `http://localhost:${port}/imgs/city.jpg`, tags: ["tech", "programming"] },
+    { id: 2, title: "Secondo post", body: "Questo è il contenuto del secondo post", img: `http://localhost:${port}/imgs/grass.jpg`, tags: ["lifestyle", "travel"] },
+    { id: 3, title: "Terzo post", body: "Questo è il contenuto del terzo post", img: `http://localhost:${port}/imgs/man.jpg`, tags: ["food", "cooking"] },
+    { id: 4, title: "Quarto post", body: "Questo è il contenuto del quarto post", img: `http://localhost:${port}/imgs/sea.jpg`, tags: ["fitness", "health"] },
+    { id: 5, title: "Quinto post", body: "Questo è il contenuto del quinto post", img: `http://localhost:${port}/imgs/street.jpg`, tags: ["fashion", "style"] }
 ];
 
 
 //Index
 router.get('/', (req, res) => {
-    res.json(posts)
+    res.json(posts);
 })
 
 //Show
 router.get('/:id', (req, res) => {
-    res.send('Post con id = ' + req.params.id);
+    const post = posts.find(post => post.id == req.params.id)
+    if (post) {
+        res.json(post)
+    } else { res.send("Non esiste nessun post con id = " + req.params.id) }
 })
 
 //Store 
